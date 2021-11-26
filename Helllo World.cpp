cf7e1d319c20f090f3316b34a35167a6ab8f14b7
Kartika Sari <92368462+Kartikasaei@users.noreply.github.com>
2021-11-26 08:08:38 +0700

    Add files via upload

---
+++ Helllo World.cpp
@@ -0,0 +1,29 @@
+/*  "HELLO WORLD"
+============================================================================
+Program ini dibuat sebagai pengenalan awal program C++
+oleh Kartika Sari, ILKOMP C UNILA 2021
+pada tanggal 6 September 2021 menggunakan compiler gcc Dev C++ 
+============================================================================
+*/
+
+#include <iostream>
+/*include iostream merupakan standard library yang harus ada dalam program C++.
+yang didalamnya memuat fungsi input (cin), output (cout) dan endl*/
+
+using namespace std;
+/*using namespace std untuk menyingkat penulisan program
+
+tanda titik dua (;) merupakan hal yang harus ada untuk setiap akhir baris program,
+kalau tidak ada maka program akan error*/ 
+
+int main(){
+	/*pada C++ setidaknya terdapat 1 fungsi, kita buat int main.
+	kurung kurawal buka sebagai tanda awal dari sebuah program.
+	lalu ditutup dengan kurawal lagi*/
+	 
+	cout<<"Hello World"<<endl;
+	/*cout (consol out) berfungsi sebagai fungsi output yang mengeluarkan 
+	apa saja yang ada didalam tanda petik. tanda panah mengarah ke kiri
+	
+	endl (end line) digunakan untuk mengakhiri baris dan pindah ke baris selanjutnya*/
+}
